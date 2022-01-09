58b78bfa82c5b2840c0e57df3e3fdb81271b7cd4
Aaron Huang <aaronhuang822@gmail.com>
2022-01-09 00:58:41 -0500

    Update HCI.py

--- HCI.py
+++ HCI.py
@@ -37,37 +37,57 @@ def listen(file_name, duration):
     print('Recording')
     sd.wait()  # Wait until recording is finished
     print('Done')
-    write(file_name, fs, myrecording)  # Save as WAV file 
+    write(file_name, fs, myrecording)  # Save as WAV file
+
+def start_inquiry(file_name, phrase):
+    text = speech_to_text(file_name)
+    if text == phrase:
+        return True
+
+def listen_inquiry(file_name):
+    listen(file_name, 3)
+    text = speech_to_text(file_name)
+    print(f'What it thinks you said: {text}')
+    return text.split()
+
+def guess_user_want(text_list):
+    guess = {}
+    for word in text_list:
+        if word.lower() in keywords_dict.keys():
+            if keywords_dict[word] not in guess.keys():
+                guess[keywords_dict[word]] = 1
+            guess[keywords_dict[word]] += 1
+    return sorted(guess, key=guess.get)[-1]
+
+def joke_inquiry():
+    joke = get_random_joke()
+    speak(joke['setup'])
+    time.sleep(1)
+    speak(joke['punchline'])
+
+def weather_inquiry():
+    data = get_weather()
+    weather = data["current"]["weather"][0]['description']
+    temperature = data["current"]["temp"]
+    weather_report = f"It's currently {weather} and {temperature} degrees celsius"
+    speak(weather_report)
 
 def main():
     while 1:
         file_name = 'temp/output.wav'
         listen(file_name, 2)
         try:
-            text = speech_to_text(file_name)
-            print(f'What it thinks you said: {text}')
-            text_list = text.split()
-            #print(text_list)
-            guess = {}
-            for word in text_list:
-                if word.lower() in keywords_dict.keys():
-                    if keywords_dict[word] not in guess.keys():
-                        guess[keywords_dict[word]] = 1
-                    guess[keywords_dict[word]] += 1
-            best_guess = sorted(guess, key=guess.get)[-1]
-            print(f'It thinks you want: {best_guess}')
-            if best_guess == 'joke':
-                joke = get_random_joke()
-                speak(joke['setup'])
-                time.sleep(1)
-                speak(joke['punchline'])
-            elif best_guess == 'weather':
-                data = get_weather()
-                weather = data["current"]["weather"][0]['description']
-                temperature = data["current"]["temp"]
-                weather_report = f"It's currently {weather} and {temperature} degrees celsius"
-                speak(weather_report)
+            if start_inquiry(file_name, 'hey maple') == True:
+                speak('Yes?')
+                text_list = listen_inquiry(file_name)
+                #print(text_list)
+                best_guess = guess_user_want(text_list)
+                print(f'It thinks you want: {best_guess}')
+                if best_guess == 'joke':
+                    joke_inquiry()
+                elif best_guess == 'weather':
+                    weather_inquiry()
         except:
-            continue
+            print('Nothing was heard')
 
 main()
